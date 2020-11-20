83a3ddeaf7245f36325dbb169c1a81258135ca71
LaurenceB28 <laurence.brd@gmail.com>
2020-11-20 14:15:11 +0100

    exo1phpP4

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo1phpP4</title>
+</head>
+<body>
+<?php
+
+function chainedeCaractere($parameter)
+{
+    return $parameter ;
+}
+
+// chainedeCaractere ('J\'adore le PHP');
+
+$phrase = chainedeCaractere('J\'adore le PHP');
+echo $phrase
+
+?>
+</body>
+</html>
